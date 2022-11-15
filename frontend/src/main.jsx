6902e7ce514691ab635fd526d5728ae53353e5cf
alex <rwrx@foxmail.com>
2022-11-15 10:36:26 +0800

    feat: replace BrowserRouter with HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import {createRoot} from "react-dom/client"
 import "./style.css"
 import App from "./App"
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
-    <BrowserRouter>
+    <HashRouter basename={"/"}>
         <App/>
-    </BrowserRouter>,
+    </HashRouter>,
 )
